5ebc30c9d55fac26b19e420d66495d5d610fd9e7
IvazereP26 <ivanmartinperez2601@gmail.com>
2022-11-16 10:57:02 +0100

    Añadiendo contador de goles, respuesta de true/false por si/no, cambio automatico de true/false

--- Jugador.java
+++ Jugador.java
@@ -24,15 +24,34 @@ public class Jugador {
         nombre = nombreDelJugador;
     }
     public void setNumeroGoles(int numeroDeGoles){
-        goles = numeroDeGoles;
+        goles += numeroDeGoles;
     }
-    public void setEsTitular(boolean siEsTitular){
-        titular = siEsTitular;
+    public void setEsTitular(){
+        if (titular == true){
+            titular = false;
+        }
+        else{
+            titular = true;
+        }
     }
     public void imprimirEstadisticas(){
-        System.out.println("El jugador " + nombre + " a marcado " + goles + " a dia de hoy, pero... ¿a sido titular?, la respuesta es... " + titular);
+        String titularidad = null;
+        if (titular == true){
+            titularidad = "sí.";
+        }
+        else{
+            titularidad = "no.";
+        }    
+        System.out.println("El jugador " + nombre + " a marcado " + goles + " a dia de hoy, pero... ¿a sido titular?, la respuesta es... " + titularidad);
     }
     public String devolverEstadisticas(){
-        return ("El jugador " + nombre + " a marcado " + goles + " a dia de hoy, pero... ¿a sido titular?, la respuesta es... " + titular);
+                String titularidad = null;
+        if (titular == true){
+            titularidad = "sí.";
+        }
+        else{
+            titularidad = "no.";
+        }   
+        return ("El jugador " + nombre + " a marcado " + goles + " a dia de hoy, pero... ¿a sido titular?, la respuesta es... " + titularidad);
     }
 }
